635ceaeeba7d296a60230067b94fb54479f5f39f
Nick Fode <dev@nfode.de>
2022-02-14 15:33:52 +0100

    Minor fixes

--- src/main/kotlin/com/example/TestController.kt
+++ src/main/kotlin/com/example/TestController.kt
@@ -1,14 +1,17 @@
 package com.example
 
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
 import reactor.core.publisher.Mono
 
-@Controller("/test")
+@Controller("/")
 class TestController {
 
-    @Post("/post")
-    fun test(): Mono<String> {
-        return Mono.just("Hello World!")
+    @Post("test")
+    fun test(@Body body: Message): Mono<String> {
+        return Mono.just(body.message)
     }
 }
+
+data class Message(val message: String)
